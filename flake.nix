bdfae7499cbd92cad862320cf303f743ac23f156
Carlos-Marques <carlosmarques.personal@gmail.com>
2023-10-05 00:17:42 +0100

    First 3 days of advent of code, both stars for each

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nortech monorepo";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+
+    makeDevShell = system: let
+      pkgs = import nixpkgs { inherit system; };
+      isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
+      darwinPackages = if isDarwin then [ ] else [ ];
+    in
+    pkgs.mkShell {
+      buildInputs = [
+        pkgs.rustup
+      ] ++ darwinPackages;
+
+      shellHook = '''';
+    };
+
+  in {
+    devShell = {
+      x86_64-linux = makeDevShell "x86_64-linux";
+      x86_64-darwin = makeDevShell "x86_64-darwin";
+      aarch64-darwin = makeDevShell "aarch64-darwin";
+    };
+  };
+}
